dad0bdce6b92ce718f5975a28569c8e25a06fbae
CharlesChinedum <charleschinedum2@gmail.com>
2025-09-13 08:11:50 +0100

    update: use console.log() in the setTimeOut in test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 console.log("Starting tests...");
 
 setTimeout(() => {
-  helpers.logger("Waiting 3 seconds...");
+  console.log("Waiting 3 seconds...");
 }, 3000);
 
 console.log("Tests completed");
